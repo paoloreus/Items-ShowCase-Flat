a77854b8299a906ffa1dc93d39363d11656a3b4c
Videospirit <73370893+Videospirit@users.noreply.github.com>
2020-10-27 20:08:41 -0400

    Add files via upload

---
+++ methods.php
@@ -0,0 +1,31 @@
+<?php
+function getProducts(){
+    $file=fopen('products.csv','rb');
+    $products = array();
+    while(!feof($file)){
+        $product =fgetcsv($file);
+        if($product===false)continue;
+        $products[]=$product;
+    }
+    return $products;
+}
+
+function saveProducts(array $products){
+    $file = fopen('products.csv','wb');
+    foreach($products as $product){
+        fputcsv($file,$product);
+    }
+    fclose($file);
+}
+
+function deleteProduct(array $products,$pid){
+    for($x = 0;$x<count($products);$x++) {
+        if ($products[$x][0] == $pid) {
+            array_splice($products,$x,1);
+        }
+    }
+    return $products;
+}
+
+
+?>
