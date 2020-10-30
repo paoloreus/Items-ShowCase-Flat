b78b5583079ade63c93a4baad23151316b5e4f73
Paolo <63888595+paolotous@users.noreply.github.com>
2020-10-30 14:23:16 -0400

    Add files via upload

---
+++ uploadHome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Upload Image
+    <input type="file" name="upload" id="upload">
+    <input type="submit" value="Upload" name="submit">
+
+</form>
+
+</body>
+
+</html>
